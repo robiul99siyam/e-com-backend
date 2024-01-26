1a6b3e8bb044acbff229955526bd1ec100acc8a6
robiul99siyam <siyamrobiulislam@gmail.com>
2024-01-26 15:13:18 +0600

    'e-com'

--- e_comarce/settings.py
+++ e_comarce/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
